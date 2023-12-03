e972d855ab614aa5421699b1d9ae380bdd30b3df
sarahdoherty1 <142939713+sarahdoherty1@users.noreply.github.com>
2023-12-03 12:45:57 -0500

    Web3 (#7)
    * Return Example Json
    
    * Render HTML files
    
    * shared layout
    
    * implemented bootstrap styles
    
    * Recap, carousel
    
    * Unemployment report with flash messaging
    
    * Stocks functionality
    
    * import stocks route variable
    
    * gunicorn in requirements
    
    * stocks dashboard
    
    * configure server

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,6 +2,9 @@
 # this is the "web_app/__init__.py" file...
 import os
 
+
+import os
+
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
@@ -19,7 +22,6 @@ def create_app():
     app.register_blueprint(home_routes)
     app.register_blueprint(unemployment_routes)
     app.register_blueprint(stocks_routes)
-
     return app
 
 if __name__ == "__main__":
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -8,7 +8,6 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
     return render_template("home.html")
 
 @home_routes.route("/about")
